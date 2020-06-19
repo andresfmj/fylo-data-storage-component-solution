bea65e30f7d07e05f7e29c9c16ee8020d2415454
Andres Fernando <andres.fernando08@gmail.com>
2020-06-19 10:23:07 -0500

    refactor and it use decoupled components on main one

--- src/components/Fylo.js
+++ src/components/Fylo.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 import '../styles/fylo/fylo.styles.scss'
 
-import logo from '../images/logo.svg'
-
-import ic_document from '../images/icon-document.svg'
-import ic_folder from '../images/icon-folder.svg'
-import ic_upload from '../images/icon-upload.svg'
+import LeftMenu from './UI/LeftMenu/LeftMenu'
+import RightStats from './UI/RightStats/RightStats'
 
 
 function Fylo(props) {
@@ -14,35 +11,8 @@ function Fylo(props) {
     return (
         <div className='Fylo'>
             <div className="fylo-wrapper">
-                <div className="left-menu">
-                    <div className="logo-header">
-                        <img src={logo} alt="Brand" />
-                    </div>
-                    <div className="left-menu-inner">
-                        <ul>
-                            <li><img src={ic_document} alt="New Document" /></li>
-                            <li><img src={ic_folder} alt="New Folder" /></li>
-                            <li><img src={ic_upload} alt="Upload" /></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="right-stats">
-                    <p>You've used <span className="highlighted">815 GB</span> of your storage</p>
-                    <div className="float-legend">
-                        <h1>185 <span className="text-small">gb left</span></h1>
-                    </div>
-                    <div className="progress-container">
-                        <div className="progress">
-                            <div className="progress-inner">
-                                <div className="progress-dot"></div>
-                            </div>
-                        </div>
-                        <div className="progress-text">
-                            <p>0 gb</p>
-                            <p>1000 gb</p>
-                        </div>
-                    </div>
-                </div>
+                <LeftMenu />
+                <RightStats />
             </div>
         </div>
     )
