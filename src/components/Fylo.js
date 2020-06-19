233ce7e9e7626ac100d9a54f6c83ec8244408e0c
Andres Fernando <andres.fernando08@gmail.com>
2020-06-18 22:23:17 -0500

    fixed styles, added a container to wrap left and right components

--- src/components/Fylo.js
+++ src/components/Fylo.js
@@ -13,32 +13,34 @@ function Fylo(props) {
 
     return (
         <div className='Fylo'>
-            <div className="left-menu">
-                <div className="logo-header">
-                    <img src={logo} alt="Brand" />
-                </div>
-                <div className="left-menu-inner">
-                    <ul>
-                        <li><img src={ic_document} alt="New Document" /></li>
-                        <li><img src={ic_folder} alt="New Folder" /></li>
-                        <li><img src={ic_upload} alt="Upload" /></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="right-stats">
-                <p>You've used <span className="highlighted">815 GB</span> of your storage</p>
-                <div className="float-legend">
-                    <h1>185 <span className="text-small">gb left</span></h1>
-                </div>
-                <div className="progress-container">
-                    <div className="progress">
-                        <div className="progress-inner">
-                            <div className="progress-dot"></div>
-                        </div>
+            <div className="fylo-wrapper">
+                <div className="left-menu">
+                    <div className="logo-header">
+                        <img src={logo} alt="Brand" />
                     </div>
-                    <div className="progress-text">
-                        <p>0 gb</p>
-                        <p>1000 gb</p>
+                    <div className="left-menu-inner">
+                        <ul>
+                            <li><img src={ic_document} alt="New Document" /></li>
+                            <li><img src={ic_folder} alt="New Folder" /></li>
+                            <li><img src={ic_upload} alt="Upload" /></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="right-stats">
+                    <p>You've used <span className="highlighted">815 GB</span> of your storage</p>
+                    <div className="float-legend">
+                        <h1>185 <span className="text-small">gb left</span></h1>
+                    </div>
+                    <div className="progress-container">
+                        <div className="progress">
+                            <div className="progress-inner">
+                                <div className="progress-dot"></div>
+                            </div>
+                        </div>
+                        <div className="progress-text">
+                            <p>0 gb</p>
+                            <p>1000 gb</p>
+                        </div>
                     </div>
                 </div>
             </div>
